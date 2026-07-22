54b51fa6ed9168c0c33c82d2fe6e9b408a9ddfb3
Ayman Baig <aymanbg82@gmail.com>
2026-07-22 21:56:13 +0530

    Use Firebase environment variable

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,12 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Get the project folder
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Build the full path to the JSON file
-key_path = os.path.join(BASE_DIR, "firebase", "serviceAccountKey.json")
-
-print("Using key:", key_path)
+firebase_credentials = json.loads(os.environ["FIREBASE_CREDENTIALS"])
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(key_path)
+    cred = credentials.Certificate(firebase_credentials)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
